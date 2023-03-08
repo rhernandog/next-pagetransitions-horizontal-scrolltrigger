c0fa5aa965c8300a05e2dfc0c803149b0aa52809
Rodrigo Hernando <rhernandog@gmail.com>
2023-03-08 12:56:21 -0300

    latest update

---
+++ pages/scroll.js
@@ -0,0 +1,42 @@
+import { useRef } from 'react'
+import gsap from "gsap/dist/gsap";
+
+import { useIsomorphicLayoutEffect } from '../helpers/isomorphicEffect';
+
+export default function Scroll() {
+  const main = useRef();
+  
+  useIsomorphicLayoutEffect(() => {
+    const ctx = gsap.context((self) => {
+      const boxes = self.selector(".box");
+      boxes.forEach((box) => {
+        gsap.to(box, { 
+          x: 300,
+          scrollTrigger: {
+            trigger: box,
+            start: "center center",
+            end: "top 20%",
+            pin: true,
+            scrub: true,
+          }
+        });
+      });
+    }, main);
+    return () => ctx.revert();
+  }, []);
+  
+  return (
+    <div>
+      <section className="section flex-center column">
+        <h1>Basic ScrollTrigger with React</h1>
+        <h2>Scroll down to see the magic happen!!</h2>
+      </section>
+      <div className="section flex-center column" ref={main}>
+        <div className="box">box</div>
+        <div className="box">box</div>
+        <div className="box">box</div>
+      </div>
+      <section className="section"></section>
+    </div>
+  );
+}
